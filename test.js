6be73a661ad3ed1042ec77d90609c7ea8479ccdf
kalium <jehansingh.97@gmail.com>
2017-06-24 00:38:15 +0200

    Created parsing system

--- test.js
+++ test.js
@@ -11,18 +11,36 @@
 console.log("Starting test run");
 
 var dict = {};
+var block = {};
 
-var options = {
+var options = { 
     url: 'https://api.github.com/search/users?q=location:CapeTown&sort=followers&order=desc',
-    headers: {'User-Agent' : 'request'}
+    //Calls the API, requests all users in Cape Town sorted by follower count
+    headers: {'User-Agent' : 'request'} //Satisfies Github's need for a user agent
 };
 
-var request = require('request');
-request(options,function(error,response,body){
+
+var request = require('request'); //load request module
+request(options,function(error,response,body){ //make request, pass values into function
 
 
-    console.log(body);
+    //console.log(body);
+    /*
+    var out = body.split("},{");
+    console.log(out[0]);
+    */
+
+    var parseable = JSON.parse(body);
+
+    for (var i = 0; i < parseable.items.length; i++){
+    
+        var item = parseable.items[i];
+        block[i] = parseable.items[item.followers_url] = item.login;
+
+    }
+
+    console.log(block);
 
 });
+//it's parsing time!
 
-JSON.parse
