c504a6d9b9ea49d699dd28bc9dd6d6dbd20175c4
kalium <jehansingh.97@gmail.com>
2017-07-24 13:03:57 +0200

    Add parsing and splitting for javascript, waiting for github timeout to expire

--- refactor.js
+++ refactor.js
@@ -18,11 +18,24 @@ userURL = 'https://api.github.com/search/users?q=location:CapeTown&sort=follower
 get_promise(userURL).then(function(users){
  
     users = users.items;
-    users.map(function(user){
+    users = users.map(function(user){
         
-        console.log(user.login);
+        return([user.followers_url,user.login]);
+        
+    });
+
+
+    output = users.map(function(userblock){
+
+        get_promise(userblock[0]).then(function(data){
+    
+            return([data.length,userblock[1]]);
+
+        });
 
     });
+
+    list.slice(0,10).map(function(line) { console.log(line[0]+" "+line[1]);
 
 });
 
