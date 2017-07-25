585f892780209936b0f0a07ae78be9a293c089be
kalium <jehansingh.97@gmail.com>
2017-07-25 10:27:04 +0200

    remove awful pairing mapping code, make neater

--- refactor.js
+++ refactor.js
@@ -3,7 +3,7 @@ function get_promise(requestUrl){
     var scrapeOptions = {
         url: requestUrl,
         qs: {
-            access_token: "50ee2e9f453a3a7718bbaff2aa6babcb4c876df7" //REMOVE BEFORE COMMIT
+            access_token: "3" //REMOVE BEFORE COMMIT
         },
         headers: { 'User-Agent':'request'},
         json:true
@@ -21,7 +21,7 @@ get_promise(userURL).then(function(users){
     users = users.items;
     users = users.map(function(user){
         
-        return([user.url,user.login]);
+        return([user.url, user.login]);
         
     });
 
@@ -32,7 +32,7 @@ get_promise(userURL).then(function(users){
         
     stack = users.map(function(user){
         
-        pair.push(user[1])
+        //pair.push(user[1])
         return (get_promise(user[0]));
         
     });
@@ -40,16 +40,16 @@ get_promise(userURL).then(function(users){
     Promise.all(stack).then(data => {
        
         var output = [];
-
+        /*
         output = pair.map(function(x){
 
            return [x];
 
-        });
+        });*/
 
-        data.map(function(x){
+        output = data.map(function(x){
 
-            output[data.indexOf(x)].push(x.followers);
+            return([x.followers,x.login]);
 
         });
 
